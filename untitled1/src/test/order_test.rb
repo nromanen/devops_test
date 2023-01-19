6b7db6983a44e0d3219c4b4ad143eaab16219d83
Nataliia <nataliia@Nataliias-MBP.localdomain>
2023-01-19 08:44:52 +0200

    initial commit

---
+++ untitled1/src/test/order_test.rb
@@ -0,0 +1,33 @@
+# def test_making_order
+#   book = Book.new(:title => "RSpec Intro", :price => 20)
+#   customer = Customer.new
+#   order = Order.new(customer, book)
+#   puts 'kkkk'
+#   order.submit
+#
+#   assert(customer.orders.last == order)
+#   assert(customer.ordered_books.last == book)
+#   assert(order.complete?)
+#   assert(!order.shipped?)
+# end
+require 'test/unit'
+require_relative '../script'
+
+class OrderTest < Test::Unit::TestCase
+  # def setup
+  # end
+
+  # def teardown
+  # end
+
+  def test_fail
+    assert(false , 'Assertion was false.')
+  end
+
+  def test_script
+    expected = 2
+    sc = Script.new
+    actual = sc.my_method
+    assert_equal(expected, actual)
+  end
+end
