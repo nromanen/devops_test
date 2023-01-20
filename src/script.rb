deda4afbe9489ed452a0513084ba9b9a2c681ceb
Nataliia <nromanen.softserveinc@gmail.com>
2023-01-20 16:14:41 +0200

    add some tests \n refactor code

--- src/script.rb
+++ src/script.rb
@@ -1,80 +1,74 @@
 require 'net/http'
 require 'uri'
 require 'json'
-  class GithubApi
+class GithubApi
 
-    @@repo_uri =  'https://api.github.com/repos/nromanen/pratical_testing_2022'
-    @@token = ''
-    def get(url)
-      uri = URI.parse("#{@@repo_uri}#{"/#{url}" if url != ''}")
-      request = Net::HTTP::Get.new(uri)
-      request["Accept"] = "application/vnd.github+json"
-      request["Authorization"] = "Bearer #{@@token}"
-      request["X-Github-Api-Version"] = "2022-11-28"
+  def initialize(repo_uri, token)
+    @repo_uri =  repo_uri
+    @token = token
+  end
+  def get(url)
+    uri = URI.parse("#{@repo_uri}#{"/#{url}" if url != ''}")
+    request = Net::HTTP::Get.new(uri)
+    request["Accept"] = "application/vnd.github+json"
+    request["Authorization"] = "Bearer #{@token}"
+    request["X-Github-Api-Version"] = "2022-11-28"
 
-      req_options = {
-        use_ssl: uri.scheme == "https",
-      }
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
 
-      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-        http.request(request)
-      end
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
     end
-
-    def branch_protected?(json_obj, branch_name)
-      branches = JSON.parse(json_obj).select{|element| element["name"] == branch_name}
-      return "Branch with #{branch_name} doesn't exist" if branches.empty?
-      branches[0]["protected"]
-    end
-
-    def branch_exist?(json_obj, branch_name)
-      !JSON.parse(json_obj).select{|element| element["name"] == branch_name}.empty?
-    end
-
-    def branches(json_obj)
-      JSON.parse(json_obj)
-    end
-
-    def branch(json_obj, branch_name)
-      JSON.parse(json_obj).select{|element| element["name"] == branch_name}
-    end
-
-    def default_branch(json_obj)
-      JSON.parse(json_obj)["default_branch"]
-    end
-
   end
 
+  def branch_protected?(branch_name)
+    branches = JSON.parse(get('branches').body).select{|element| element["name"] == branch_name}
+    return "Branch with #{branch_name} doesn't exist" if branches.empty?
+    branches[0]["protected"]
+  end
 
-  obj = GithubApi.new
-json_obj = obj.get('branches')
-p json_obj
-# p obj.default_branch(json_obj)
+  def branch_exist?(branch_name)
+    !JSON.parse(get('branches').body).select{|element| element["name"] == branch_name}.empty?
+  end
 
-  # puts obj.branch_protected?(response_body, 'main')
-  # puts obj.branch_exist?(response_body, 'main')
-  # puts obj.branches(response_body)
+  def branches
+    JSON.parse(get('branches').body)
+  end
 
-  # puts !obj.get('https://api.github.com/repos/nromanen/pratical_testing_2022/contents/.github/ISSUE_TEMPLATE/test-case-template.md', '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').body.nil?
-  #puts obj.get('https://api.github.com/repos/nromanen/pratical_testing_2022/contents/state_transition.pdf?ref=DashaFomenko', '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').body
+  def branch(json_obj, branch_name)
+    JSON.parse(json_obj).select{|element| element["name"] == branch_name}
+  end
 
-  # puts obj.get('https://api.github.com/repos/nromanen/pratical_testing_2022/contents/CODEOWNERS?ref=main', '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').body
+  def default_branch
+    JSON.parse(get('').body)["default_branch"]
+  end
+  def file_branch(file_name, branch_name)
+    return nil if get("contents/#{file_name}?ref=#{branch_name}").code != '200'
+    old_uri = @repo_uri
+    @repo_uri =  old_uri.gsub('api.github.com/repos', 'raw.githubusercontent.com')
+    result = get("#{branch_name}/#{file_name}").body
+    @repo_uri = old_uri
+    result
+    # "download_url": "https://raw.githubusercontent.com/nromanen/pratical_testing_2022/main/CODEOWNERS",
+  end
 
-  # puts JSON.parse(obj.get('https://api.github.com/repos/nromanen/pratical_testing_2022', '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').body)["default_branch"]
+  def rules_required_pull_request_reviews(branch_name)
+    response = get("branches/#{branch_name}/protection")
+    return nil  if response.code != '200'
+    JSON.parse(response.body)["required_pull_request_reviews"]
 
-  # puts obj.branch(response_body, 'develop')
-  # hash = JSON.parse(response_body)
-  # puts hash
+    # obj.get('https://api.github.com/repos/nromanen/pratical_testing_2022/branches/develop/protection', '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').body
+    # "required_pull_request_reviews":{
+    #   "url":"https://api.github.com/repos/nromanen/pratical_testing_2022/branches/develop/protection/required_pull_request_reviews",
+    #   "dismiss_stale_reviews":false,
+    #   "require_code_owner_reviews":false,
+    #   "require_last_push_approval":false,
+    #   "required_approving_review_count":2
+    # },
+  end
 
-  #puts obj.get('https://api.github.com/repos/nromanen/pratical_testing_2022/branches/develop/protection', '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').body
-  # "required_pull_request_reviews":{
-  #   "url":"https://api.github.com/repos/nromanen/pratical_testing_2022/branches/develop/protection/required_pull_request_reviews",
-  #   "dismiss_stale_reviews":false,
-  #   "require_code_owner_reviews":false,
-  #   "require_last_push_approval":false,
-  #   "required_approving_review_count":2
-  # },
-  # response.code
-  # response.body
+end
 
 
